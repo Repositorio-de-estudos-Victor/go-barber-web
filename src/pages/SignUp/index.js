e4508fe658fb8ed850daa5af96c188ac4b124301
Victor Novais <victor96novais@gmail.com>
2019-11-14 14:39:44 -0300

    :rocket: Notifications

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,23 +1,28 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
+import { signUpRequest } from '~/store/modules/auth/actions';
+
 import logo from '~/assets/logo.svg';
 
-export default function signUp() {
-  const schema = Yup.object().shape({
-    name: Yup.string().required('O nome é obrigatório'),
-    email: Yup.string()
-      .email('Insira um e-mail válido')
-      .required('O e-mail é obrigatório'),
-    password: Yup.string()
-      .min(6, 'A senha deve conter no minímo 6 caractéres')
-      .required('A senha é obrigatória'),
-  });
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'A senha deve conter no minímo 6 caractéres')
+    .required('A senha é obrigatória'),
+});
 
-  function handleSubmit(data) {
-    console.tron.log(data);
+export default function SignUp() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
   }
 
   return (
