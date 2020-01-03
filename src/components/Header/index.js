0ca2302b68eda90f7187410b8697b921317a9a29
Victor Rodrigues Novais <victor96novais@gmail.com>
2020-01-03 16:33:14 -0300

    :art: tratamento no avatar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,6 +10,10 @@ import { Container, Content, Profile } from './styles';
 export default function Header() {
   const profile = useSelector(state => state.user.profile);
 
+  const avatar = profile.avatar
+    ? profile.avatar.url
+    : 'https://api.adorable.io/avatars/50/abott@adorable.png';
+
   return (
     <Container>
       <Content>
@@ -26,13 +30,7 @@ export default function Header() {
               <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
-            <img
-              src={
-                profile.avatar.url ||
-                'https://api.adorable.io/avatars/50/abott@adorable.png'
-              }
-              alt="Victor Novais"
-            />
+            <img src={avatar} alt="Victor Novais" />
           </Profile>
         </aside>
       </Content>
